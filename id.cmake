44998c95cf73676f18bdc12d623e9de9ee723f5c
David Callu <callu.david@gmail.com>
2017-12-04 17:21:07 +0100

    feat(version): update to 1.5.2

--- id.cmake
+++ id.cmake
@@ -1,14 +1,14 @@
 set(package libjpeg_turbo)
-set(version 1.5.1)
+set(version 1.5.2)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-1
+    VERSION ${version}-2
     ABI ${version}
     )
 
 byd__package__download_info(${package}
-    URL "https://sourceforge.net/projects/libjpeg-turbo/files/1.5.1/libjpeg-turbo-1.5.1.tar.gz"
-    URL_HASH SHA1=ebb3f9e94044c77831a3e8c809c7ea7506944622
+    URL "https://sourceforge.net/projects/libjpeg-turbo/files/1.5.2/libjpeg-turbo-1.5.2.tar.gz"
+    URL_HASH SHA1=e788f6defa58b4393a5e1685c018f3b962971457
 )

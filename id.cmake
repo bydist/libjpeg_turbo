6cc4a63e9991cc918cac6009fedf333b22e92102
David Callu <callu.david@gmail.com>
2018-03-03 19:44:54 +0100

    feat(id): use source from github

--- id.cmake
+++ id.cmake
@@ -4,11 +4,11 @@ set(version 1.5.2)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-2
+    VERSION ${version}-3
     ABI ${version}
     )
 
 byd__package__download_info(${package}
-    URL "https://sourceforge.net/projects/libjpeg-turbo/files/1.5.2/libjpeg-turbo-1.5.2.tar.gz"
-    URL_HASH SHA1=e788f6defa58b4393a5e1685c018f3b962971457
+    URL "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/1.5.2.tar.gz"
+    URL_HASH SHA1=af344620ed324340259c2f2f8ec30d12dbed384a
 )

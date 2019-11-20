aaa0dbe60e878cd7096f13fb4bd03223dd7fab2c
David Callu <callu.david@gmail.com>
2019-11-20 10:14:21 +0100

    feat(id): bump to 2.0.3

--- id.cmake
+++ id.cmake
@@ -1,14 +1,14 @@
 set(package libjpeg_turbo)
-set(version 1.5.2)
+set(version 2.0.3)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-5
-    ABI 1.5
+    VERSION ${version}-1
+    ABI 2.0
     )
 
 byd__package__download_info(${package}
-    URL "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/1.5.2.tar.gz"
-    URL_HASH SHA1=af344620ed324340259c2f2f8ec30d12dbed384a
+    URL "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/2.0.3.tar.gz"
+    URL_HASH SHA1=8bc6421a28a3855a1d93df3259edf17367ae4792
 )
